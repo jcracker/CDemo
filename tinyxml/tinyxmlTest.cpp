654cf5109982c1b76cb2e5ed5d6c3b1579e79e9a
jcracker <jcracker@163.com>
2013-02-06 04:46:52 -0500

    create tinyxml demo code

---
+++ tinyxml/tinyxmlTest.cpp
@@ -0,0 +1,30 @@
+#include "tinyxml.h"
+#include "tinystr.h"
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	TiXmlDocument *myDocument = new TiXmlDocument("info.xml");
+	myDocument->LoadFile();
+
+	TiXmlElement *RootElement = myDocument->RootElement();
+	cout<<RootElement->Value()<<endl;
+
+	TiXmlElement *FirstPerson = RootElement->FirstChildElement();
+	cout<<FirstPerson->Value()<<endl;
+
+	TiXmlElement *NameElement = FirstPerson->FirstChildElement();
+	TiXmlElement *AgeElement = NameElement->NextSiblingElement();
+
+	TiXmlAttribute *IDAttribute = FirstPerson->FirstAttribute();
+
+	cout<<NameElement->FirstChild()->Value()<<endl;
+	cout<<AgeElement->FirstChild()->Value<<endl;
+	cout<< IDAttribute->Value()<<endl;
+
+
+	return 0;
+}
