c9207a82d65514aaecbc396cacb46a7251012041
jcracker <jcracker@163.com>
2013-02-06 02:27:27 -0500

    modify folder

---
+++ db/insert.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <mysql/mysql.h>
+#include <string.h>
+using namespace std;
+main()
+{
+	MYSQL mysql;
+	mysql_init(&mysql);
+	mysql_real_connect(&mysql,"localhost","root","root","cplus",3306,NULL,0);
+	string sql="insert into user(username,password) values('wangpeng','password');";
+	mysql_query(&mysql,sql.c_str());
+	mysql_close(&mysql);
+	//demo
+}
